81b93be32b066c7135819b1a73061e15565fb336
Emanuel Lindström <emanuel.lindstrom@gmail.com>
2024-12-06 22:41:19 +0100

    5.1 test input working. Not actual input.

---
+++ 2024/4.2/src/main.rs
@@ -0,0 +1,247 @@
+use std::{fs::read_to_string, ops::Index};
+
+fn main()  {
+
+    // 1. Create a vector of all rows. 
+    // 2. Create a vector of all columnn.
+    // 3. Create a vector of all diagonals. (x2)
+    // Use the row vectors to create the diagonals.
+    // 4. Search through each vector, backwards and forwards to match each word. 
+
+    // (x,y) = (n,m)
+    //                                       n,m - n+1,m+1 - n+2,m+2 
+    // To create each diagnonal, go from pos 0,0 - 1,1 - 2,2 - 3,3 up to n = line_length
+    //                        skip first(!X,!Y -) !n-1, !m-1 
+
+    let file_path = "./input.txt";
+    // let mut rows:   Vec<Vec<String>> = vec![];
+    let mut columns: Vec<Vec<String>> = vec![];
+    // let mut diagonals: Vec<Vec<String>> = vec![];
+
+    let mut a_pos = vec![];
+    
+
+    let mut line_len = 0;
+    for (line_index, line) in read_to_string(file_path).unwrap().lines().enumerate() {
+        if line_len == 0 {
+            line_len = line.len();
+        }
+            
+            for column_index in 0..line.len(){
+                if line_index == 0 {
+                    // Create a new vec and add to columns.
+                    columns.push(vec![]);
+                }
+
+                let character = line.chars().nth(column_index).unwrap();
+                columns[column_index].push(character.to_string());
+
+                if character == 'A' && line_index != 0 && line_index != line_len-1 && column_index != 0 && column_index != line_len-1 {
+                    a_pos.push((column_index,line_index))
+
+                }
+            }
+    }
+
+    
+    // For each position in A, create 2 words and match them against MAS and SAM
+
+    let mut count= 0;
+    for (x,y) in a_pos {
+        println!("{x},{y}");
+        let val1 = columns.index(x-1).index(y-1);
+        let val2 = columns.index(x+1).index(y-1);
+        let val3 = columns.index(x-1).index(y+1);
+        let val4 = columns.index(x+1).index(y+1);
+
+        let word1 = val1.clone() + "A" + val4;
+        let word2 = val2.clone() + "A" + val3;
+
+        match (&word1[..], &word2[..]) {
+            ("MAS", "MAS") => {
+                count += 1;
+            },
+            ("SAM", "SAM") => {
+                count += 1;
+            },
+            ("MAS", "SAM") => {
+                count += 1;
+            },
+            ("SAM", "MAS") => {
+                count += 1;
+            },
+            _ => {
+                // println!("No match for {},{}",word1, word2);
+            }
+        }
+
+
+    }
+
+    println!("count: {count}");
+
+
+    // println!("Rows: {:?}", rows);
+    // println!("First column: {:?}", columns.index(0));
+    // println!("Last column: {:?}", columns.index(line_len-1));
+
+
+    // let mut first_diagonals = vec![];
+    // let mut second_diagonals = vec![];
+    // let mut third_diagonals = vec![];
+    // let mut forth_diagonals = vec![];
+    // for index in 0..line_len {
+    //     let mut first_diagonal = vec![];
+    //     let mut second_diagonal = vec![];
+    //     let mut third_diagonal = vec![];
+    //     let mut forth_diagonal = vec![];
+
+    //     // First diagonal:
+    //     let top_x_start = index;
+    //     let top_y_start = 0;
+    //     let bottom_x_start = index;
+    //     let bottom_y_start = line_len -1;
+    //     for increment in 0..line_len {
+    //         // First diagonal
+    //         let first_x = top_x_start - increment;
+    //         let first_y = top_y_start + increment;
+    //         let first_val = columns.index(first_x).index(first_y).to_string();
+    //         first_diagonal.push(first_val);
+
+    //         if first_x == 0 || first_y == (line_len -1) {
+    //             break
+    //         }
+    //     } 
+
+
+    //     for increment in 0..line_len {
+    //         // Second diagonal
+    //         let second_x = top_x_start + increment;
+    //         let second_y = top_y_start + increment;
+    //         let second_val = columns.index(second_x).index(second_y).to_string();
+    //         second_diagonal.push(second_val);
+
+    //          if second_x == (line_len -1 ) || second_y == (line_len - 1) {
+    //             break
+    //          }
+    //     }
+
+
+    //     for increment in 0..line_len {
+    //         // Third diagonal
+    //         if index == line_len -1 { 
+    //             // Skip first index, because we already have the middle diagonal from first and second diagonals
+    //             continue;
+    //          }
+    //         let third_x = bottom_x_start - increment;
+    //         let third_y = bottom_y_start - increment;
+    //         let third_val = columns.index(third_x).index(third_y).to_string();
+    //         third_diagonal.push(third_val);
+
+    //          if third_x == 0 || third_y == 0 {
+    //             break
+    //          }
+    //     }
+
+    //     for increment in 0..line_len {
+    //         // Forth diagonal
+    //         if index == 0 { 
+    //             // Skip first index, because we already have the middle diagonal from first and second diagonals
+    //             continue;
+    //          }
+    //         let forth_x = bottom_x_start + increment;
+    //         let forth_y = bottom_y_start - increment;
+    //         let forth_val = columns.index(forth_x).index(forth_y).to_string();
+    //         forth_diagonal.push(forth_val);
+
+    //          if forth_x == (line_len - 1) || forth_y == 0 {
+    //             break
+    //          }
+    //     }
+
+    //     // first_diagonals.push(first_diagonal);
+    //     // second_diagonals.push(second_diagonal);
+    //     // third_diagonals.push(third_diagonal);
+    //     // forth_diagonals.push(forth_diagonal);
+    //     // diagonals.push(first_diagonal);
+    //     // diagonals.push(second_diagonal);
+    //     // diagonals.push(third_diagonal);
+    //     // diagonals.push(forth_diagonal);
+
+    //     // println!("forth diagonal: {:?}", forth_diagonal);
+    // }
+    // // println!("Diagonals: {:?}",diagonals);
+    // // println!("columns: {:?}",columns);
+    // println!("rows: {:?}",rows);
+
+
+    // Search for XMAS & SAMX in rows, columns and diagonals
+    // let word1 = "XMAS".to_string();
+    // let word2 = "SAMX".to_string();
+    // let mut count = 0;
+
+
+    // let mut iter_vec = vec![];
+    // iter_vec.append(&mut rows);
+    // iter_vec.append(&mut columns);
+    // iter_vec.append(&mut diagonals);
+
+    // Columns ok. 3
+    // Rows ok. 5 
+    // Diagonals.. 
+    // First diagonal ok.
+    // Second diagonal 
+    
+
+    // for arr in  iter_vec {
+    //     for (index, char) in arr.clone().into_iter().enumerate() {
+
+    //         if char == "X" {
+    //             let mut match_count = 0;
+    //             for (char_index,  val) in word1.chars().enumerate() {
+    //                 if arr.len() == index + char_index {
+    //                     break
+    //                 }
+    //                 let match_val = arr.index(index+ char_index).to_string();
+                    
+    //                 if val.to_string() == match_val {
+    //                     match_count += 1;
+    //                 }
+    //             }
+    //             if match_count == word1.len() {
+    //                 count +=1;
+    //                 println!("Found {word1} in {:?}, index {index}", arr);
+    //             }
+    //         }
+
+    //         if char == "S" {
+    //             let mut match_count = 0;
+    //             for (char_index,  val) in word2.chars().enumerate() {
+    //                 if arr.len() == index + char_index {
+    //                     break
+    //                 }
+    //                 let match_val = arr.index(index+ char_index).to_string();
+    //                 let v = val.to_string();
+    //                 if v == match_val {
+    //                     match_count += 1;
+    //                 }
+    //             }
+    //             if match_count == word2.len() {
+    //                 count +=1;
+
+    //                 println!("Found {word2} in {:?}, index {index}", arr);
+    //             }
+    //         }
+    //     }
+    // }
+
+    // println!("Count: {count}"); // 2500 correct?
+       
+    }
+
+
+
+
+   
+
+
--- 2024/5.1/src/main.rs
+++ 2024/5.1/src/main.rs
@@ -1,28 +1,120 @@
-use std::fs::read_to_string;
+use std::{fs::read_to_string, ops::Index};
+use rand::thread_rng;
+use rand::seq::SliceRandom;
+
+
 
 fn main() {
+    let test = true;    
+    // Step 1: Create an arr 1 - 100 
+    // Step 2: Sort the array according to the rules.
+        // Step 2.1 For each rule, check that the arr obeys the rules.
+        // Step 2.2 If a rule is not fulfilled, move the number in the array and check again.
+        // Step 2.3 Repeat until arr is fully sorted.
+    // Step 3: Find the indexes for the update row from the array of ordered numbers 
+    // Step 4; Place them according to their index. 
+
+
+    let input_path = match test {
+         true => "test_input.txt",
+         false => "input.txt"
+    };
     
-    
-    // Pair up the smallest number in the left list 
-    // with the smallest number in the right list
-    // then the second-smallest left number with 
-    // the second-smallest right number, and so on.
-
-    // Step 1. Read the input in to two lists. 
-    // Step 2. Sort the lists in order, smallest first. 
-    // Step 3. Calculate absolute distance between numbers. 
-    // Step 4. Add all distances. 
-
-    let input_path = "./test_input.txt";
-    let instructions_path = "./test_instructions.txt";
- 
-    let input = read_to_string(input_path).unwrap().lines();
-
-    for (index,line) in read_to_string(instructions_path).unwrap().lines().enumerate() {
-        println!("Instructions: {line}", )
-
+    let updates = read_to_string(input_path).unwrap();
+    let mut rules: Vec<(u32,u32)> = vec![];
+    let instructions_path = match test {
+        true => "./test_instructions.txt",
+        false => "./instructions.txt",
+    }; //"./instructions.txt";
+    let _lines = read_to_string(instructions_path).unwrap();
+    for instructions in  _lines.lines() {
+        // println!("Instructions: {instructions}" );
+        let values: Vec<&str> = instructions.split("|").collect();
+        rules.push((values.index(0).parse::<u32>().unwrap(),values.index(1).parse::<u32>().unwrap()))
     }
+    // Create array with 100 values. 0 - 100.
+    let mut arr: Vec<u32> = (1..=100).collect();
+
+    let mut count = 0;
+    let mut iter_count = 0;
+    let mut rules_followed = 0;
+    let rules_len = rules.len();
+    while !(rules_followed >= rules_len || count >= 1000) {
+        // println!("rules followed: {rules_followed}/{rules_len}, count: {count}");
+    // rules.shuffle(&mut thread_rng());
+        
+        // println!("Break while loop? {} or {} equals: {}",rules_followed < rules_len, count < 50, rules_followed < rules_len || count < 50);
+    for rule in &rules {
+        iter_count += 1;
+        // For each rule.. 
+        // Get index of each rule number. 
+        let first_pos = arr.iter().position(|val| *val == rule.0).unwrap();
+        let second_pos = arr.iter().position(|val| *val == rule.1).unwrap();
+
+        if first_pos < second_pos {
+            // Ok!
+
+            // println!("{rules_followed}, count: {count}");
+            rules_followed += 1;
+
+            continue;
+        } else {
+            // move first_pos just before second_pos
+            // Re-order the array.
+            // We know first_pos comes after second pos, so it won't effect it's index.
+            // println!("{},{} at {},{}", rule.0, rule.1, first_pos, second_pos);
+            let moved_first_value = arr.remove(first_pos);
+            let new_index = match second_pos {
+                0 => second_pos,
+                _ => second_pos-1
+
+            };
+
+            arr.insert(new_index,moved_first_value);
+           
+            // println!("Rule {}|{} not followed! Starting over at index {iter_count}", rule.0, rule.1);
+            // println!("New arr: {:?}", arr);
+            println!(">>>>>>>>>> Breaking with {rules_followed} rules followed, {count} <<<<<<<<<<<");
+            rules_followed = 0;
+            iter_count = 0;
+            
+            break
+        }
+    }    
+    count +=1;
+
+}
+
+println!("Stopped with {rules_followed}/{rules_len} rules followed");    
+println!("===========================");
+// println!("Arr: {:?}", arr);
+
+
+// Filter out the values in the updates (input lines) from the sorted array. 
+let mut sorted_lines = vec![];
+
+
+for line in  updates.lines() {
+    let arr_clone = arr.clone();
+    // println!("Instructions: {instructions}" );
+    let values: Vec<u32> = line.split(",").map(|val| val.parse::<u32>().unwrap()).collect();
     
+     let sorted_line: Vec<u32> = arr_clone.into_iter().filter(|val| values.contains(val)).collect();
+    //  println!("Sorted line: {:?}", sorted_line);
+     sorted_lines.push(sorted_line);   
+}
+
+let mut sum = 0;
+
+for line in sorted_lines {
+    let middle_index = (line.len()-1)/2;
+    let add = line.index(middle_index);
+    sum += add;
+}
+
+println!("Sum: {sum}" )
+
+// println!("Sorted lines: {:?}", sorted_lines);
 
 
 
