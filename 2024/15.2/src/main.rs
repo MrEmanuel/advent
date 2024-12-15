6d79d38ef2555bee253884a275f259697ccee417
Emanuel Lindström <emanuel.lindstrom@gmail.com>
2024-12-15 23:04:20 +0100

    Movement is correct! Just need to calculate final score

--- 2024/15.2/src/main.rs
+++ 2024/15.2/src/main.rs
@@ -1,4 +1,9 @@
-use std::{collections::VecDeque, fs::read_to_string, io};
+use std::{
+    collections::{HashSet, VecDeque},
+    f32::consts::E,
+    fs::read_to_string,
+    io,
+};
 use utils::{DEBUG, TEST};
 
 mod utils {
@@ -86,6 +91,126 @@ fn main() {
             wait_for_input(true);
         }
     };
+
+    fn set_crates_to_move(
+        columns: &Vec<Vec<char>>,
+        crates_to_move: &mut Vec<(isize, isize)>,
+        start_pos: (isize, isize),
+        direction: (isize, isize),
+        // print_map: impl Fn(&Vec<Vec<char>>, usize, usize, Option<&Vec<OverridePos>>),
+    ) -> bool {
+        //Check next position. If
+
+        let print_map = |columns: &Vec<Vec<char>>,
+                         map_height: usize,
+                         map_width: usize,
+                         override_positions: Option<&Vec<OverridePos>>| {
+            if !DEBUG {
+                return;
+            }
+            for y_i in 0..map_height {
+                let mut line: Vec<String> = vec![];
+                for x_i in 0..map_width {
+                    // let val = columns[x_i][y_i].to_string();
+
+                    let val_to_push = match override_positions {
+                        Some(override_positions) => {
+                            match override_positions.iter().find(|pos| pos.pos == (x_i, y_i)) {
+                                Some(override_pos) => {
+                                    // override_pos.color, override_pos.mark,
+                                    let mark = match override_pos.mark {
+                                        Some(mark) => mark,
+                                        None => columns[x_i][y_i],
+                                    };
+
+                                    let res = match &override_pos.color {
+                                        Some(color) => add_color(mark, &color),
+                                        None => mark.to_string(),
+                                    };
+                                    res
+                                }
+                                None => columns[x_i][y_i].to_string(),
+                            }
+                        }
+                        None => columns[x_i][y_i].to_string(),
+                    };
+                    line.push(val_to_push);
+                }
+                if DEBUG {
+                    println!("{}", line.join(""));
+                }
+            }
+            if DEBUG {
+                wait_for_input(true);
+            }
+        };
+
+        let new_pos = (
+            start_pos.0 as isize + direction.0,
+            start_pos.1 as isize + direction.1,
+        );
+        let new_pos_mark = columns[new_pos.0 as usize][new_pos.1 as usize];
+
+        if new_pos_mark == '.' {
+            return true;
+        } else if new_pos_mark == '#' {
+            return false;
+        }
+
+        let other_half = match new_pos_mark {
+            '[' => {
+                // x+1
+                (new_pos.0 + 1, new_pos.1)
+            }
+            ']' => {
+                // x-1
+                (new_pos.0 - 1, new_pos.1)
+            }
+            _ => unreachable!("Couldn't find [ or ], found {new_pos_mark} instead.."),
+        };
+
+        let other_half_mark = columns[other_half.0 as usize][other_half.1 as usize];
+
+        if DEBUG {
+            print_map(
+                columns,
+                columns[0].len(),
+                columns.len(),
+                Some(&vec![
+                    OverridePos {
+                        color: Some("red".to_string()),
+                        mark: Some(new_pos_mark),
+                        pos: (new_pos.0 as usize, new_pos.1 as usize),
+                    },
+                    OverridePos {
+                        color: Some("red".to_string()),
+                        mark: Some(other_half_mark),
+                        pos: (other_half.0 as usize, other_half.1 as usize),
+                    },
+                ]),
+            );
+            println!(
+                "Checking {:?} and {:?} in set crates to move..",
+                new_pos, other_half
+            );
+            wait_for_input(false);
+        }
+
+        let is_ok1 = set_crates_to_move(&columns, crates_to_move, new_pos, direction);
+        let is_ok2 = set_crates_to_move(&columns, crates_to_move, other_half, direction);
+        if !is_ok1 || !is_ok2 {
+            // Don't move if any of the paths led to an obstacle.
+            return false;
+        }
+
+        if DEBUG {
+            println!("Setting crates to move inside function.. ");
+        }
+        crates_to_move.push(new_pos);
+        crates_to_move.push(other_half);
+
+        return true;
+    }
 
     let instructions_file = if TEST {
         "./test_instructions.txt"
@@ -191,7 +316,6 @@ fn main() {
                     next_step = new_pos
                 }
 
-                // TODO: Check if position is inside map.
                 match columns[new_pos.0 as usize][new_pos.1 as usize] {
                     '#' => {
                         // Can't move
@@ -249,7 +373,7 @@ fn main() {
                     }
                     '.' => {
                         if DEBUG {
-                            println!("Found free space at {:?}", new_pos);
+                            println!("horiz Found free space at {:?}", new_pos);
                             print_map(
                                 &columns,
                                 map_height,
@@ -300,8 +424,194 @@ fn main() {
                     println!("Do nothing..")
                 }
             }
+        } else {
+            // Vertical movement..
+
+            // For each increment, add crates to check in a vector.
+            // let mut crates_to_check: Vec<Vec<(usize, usize)>> = vec![];
+            // let mut increment = 0;
+            // 'increment: for _ in 1..(map_width as isize) {
+            // break if first step is a wall or free space.
+            // Go through the "pyramid"
+
+            increment += 1;
+            let vector = (direction.0 * increment, direction.1 * increment);
+            let new_pos = (
+                robot_pos.0 as isize + vector.0,
+                robot_pos.1 as isize + vector.1,
+            );
+            // if increment == 1 {
+            next_step = new_pos;
+            let new_pos_mark = columns[new_pos.0 as usize][new_pos.1 as usize];
+            match new_pos_mark {
+                // Vertical movement..
+                '#' => {
+                    // Can't move
+                    if DEBUG {
+                        println!("Found wall at {:?}", new_pos);
+                        print_map(
+                            &columns,
+                            map_height,
+                            map_width,
+                            Some(&vec![OverridePos {
+                                mark: Some(columns[new_pos.0 as usize][new_pos.1 as usize]),
+                                color: Some("red".to_string()),
+                                pos: (new_pos.0 as usize, new_pos.1 as usize),
+                            }]),
+                        );
+                    }
+                    continue 'instructions; // Go to next instruction
+                }
+                '.' => {
+                    if DEBUG {
+                        println!("vert Found free space at {:?}", new_pos);
+                        print_map(
+                            &columns,
+                            map_height,
+                            map_width,
+                            Some(&vec![OverridePos {
+                                mark: Some(columns[new_pos.0 as usize][new_pos.1 as usize]),
+                                color: Some("red".to_string()),
+                                pos: (new_pos.0 as usize, new_pos.1 as usize),
+                            }]),
+                        );
+                    }
+                    free_space_pos = Some(new_pos);
+
+                    if DEBUG {
+                        println!("Found verical free space!")
+                    }
+                }
+                // Vertical movement..
+                _ => {
+                    println!("Matched {}", new_pos_mark);
+
+                    if new_pos_mark != '[' && new_pos_mark != ']' {
+                        if DEBUG {
+                            println!("Early return..");
+                        }
+                        return;
+                    }
+
+                    let other_half = match new_pos_mark {
+                        '[' => {
+                            // x+1
+                            (new_pos.0 + 1, new_pos.1)
+                        }
+                        ']' => {
+                            // x-1
+                            (new_pos.0 - 1, new_pos.1)
+                        }
+                        _ => unreachable!("Couldn't find [ or ], found {new_pos_mark} instead.."),
+                    };
+
+                    println!("Other half: {:?}", other_half);
+
+                    // Walk the tree of crate-chars ] or [
+                    let is_ok1 =
+                        set_crates_to_move(&columns, &mut crates_to_move, new_pos, direction);
+                    let is_ok2 =
+                        set_crates_to_move(&columns, &mut crates_to_move, other_half, direction);
+                    if !is_ok1 || !is_ok2 {
+                        println!(
+                            "Crates {:?} and {:?} are NOT ok to move!",
+                            new_pos, other_half
+                        );
+                        crates_to_move = vec![];
+                        continue 'instructions; // Go to next instruction
+
+                    // Don't move if any of the paths led to an obstacle.
+                    } else {
+                        crates_to_move.push(new_pos);
+                        crates_to_move.push(other_half);
+                    }
+                }
+            }
+
+            println!(
+                "Perform vertical movement.., crates_to_move: {:?}",
+                crates_to_move
+            );
+
+            if crates_to_move.is_empty() {
+                match free_space_pos {
+                    Some(free_pos) => {
+                        // println!("Found free space at: {:?}", free_pos);
+
+                        // Update the position of the robot.
+                        columns[next_step.0 as usize][next_step.1 as usize] = '@';
+                        // Replace robot_pos with an empty space.
+                        columns[robot_pos.0][robot_pos.1] = '.';
+                        // Update robot_pos with the new one.
+                        robot_pos = (next_step.0 as usize, next_step.1 as usize);
+
+                        // print_map(&columns, map_height, map_width, None)
+                    }
+                    None => {
+                        println!("Do nothing..")
+                    }
+                }
+            } else {
+                // Perform the movements of the crates..
+                let mut moved_crates = HashSet::new();
+
+                if direction.1 < 0 {
+                    crates_to_move.sort_by(|a, b| (b.1 as usize).cmp(&(a.1 as usize)));
+                } else {
+                    crates_to_move.sort_by(|a, b| (a.1 as usize).cmp(&(b.1 as usize)));
+                }
+
+                println!("Crates to move sorted: {:?}", crates_to_move);
+
+                while !crates_to_move.is_empty() {
+                    let crate1 = crates_to_move.pop().unwrap();
+                    // let crate2 = crates_to_move.pop_front().unwrap();
+
+                    let size_before = moved_crates.len();
+                    moved_crates.insert(crate1);
+
+                    // moved_crates.insert(crate2);
+                    if moved_crates.len() == size_before {
+                        continue;
+                    }
+
+                    let crate1_mark = columns[crate1.0 as usize][crate1.1 as usize];
+                    // let crate2_mark = columns[crate2.0 as usize][crate2.1 as usize];
+                    // Move each crate 1 step in the direction..
+
+                    println!("Moving crate {}, {:?}", crate1_mark, crate1);
+                    println!("------ Crates to move sorted: {:?}", crates_to_move);
+
+                    // Move crate mark to prev pos.
+
+                    let crate1_new_pos = (crate1.0 + direction.0, crate1.1 + direction.1);
+                    // let crate2_new_pos = (crate2.0 + direction.0, crate2.1 + direction.1);
+                    println!("New pos crate1: {:?}", crate1_new_pos,);
+                    columns[crate1.0 as usize][crate1.1 as usize] = '.';
+                    // columns[crate2.0 as usize][crate2.1 as usize] = '.';
+                    columns[crate1_new_pos.0 as usize][crate1_new_pos.1 as usize] = crate1_mark;
+                    // columns[crate2_new_pos.0 as usize][crate2_new_pos.1 as usize] = crate2_mark;
+                    // prev_pos = crate_pos;
+                    // prev_mark = crate_mark;
+                    // print_map(&columns, map_height, map_width, None);
+                }
+
+                // Move robot.
+                // Update the position of the robot.
+                columns[next_step.0 as usize][next_step.1 as usize] = '@';
+                // Replace robot_pos with an empty space.
+                columns[robot_pos.0][robot_pos.1] = '.';
+                // Update robot_pos with the new one.
+                robot_pos = (next_step.0 as usize, next_step.1 as usize);
+
+                print_map(&columns, map_height, map_width, None);
+            }
+
+            // Vertical movement.. end.
         }
     }
+
+    print_map(&columns, map_height, map_width, None)
 
     // let mut total = 0;
     // for x in 0..columns.len() {
