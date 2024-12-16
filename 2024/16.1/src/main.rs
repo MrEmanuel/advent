4b96e33e1cd796ab9a517ffa1303cb4fb4c58dc5
Emanuel Lindström <emanuel.lindstrom@gmail.com>
2024-12-16 22:27:35 +0100

    Read and structure data.. not started solution yet.

--- 2024/16.1/src/main.rs
+++ 2024/16.1/src/main.rs
@@ -1,142 +1,93 @@
-use std::{collections::HashMap, fs::read_to_string};
-use utils::{print_map_animate, TEST};
+use grid::*;
+use std::{
+    fs::{self, read_to_string, File},
+    io::{self, BufRead, BufReader, Read},
+};
+#[derive(Clone, Debug)]
+struct Cell {
+    row: usize,
+    col: usize,
+    height: i32,
+    valid_neighbors: Vec<(usize, usize)>,
+}
 
-mod utils {
-    use std::{collections::HashMap, io, isize, thread, time};
-    pub const TICKER_SPEED: u64 = 40;
-    pub const DEBUG: bool = true;
-    pub const TEST: bool = true;
-    pub const VIEWPORT_HEIGHT: usize = 30;
-    pub const VIEWPORT_WIDTH: usize = 30;
-    pub const PAUSE_ON_EACH_FRAME: bool = true;
-    pub fn count_neighbors(mark: &str, pos1: (isize, isize), pos2: (isize, isize)) {}
-    pub fn wait_for_input(show_instructions: bool) {
-        if !DEBUG {
-            return;
-        }
-        let mut input = String::new();
-        if show_instructions {
-            println!("Press Enter to continue...");
-        }
-
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read input");
-    }
-    pub fn sleep() {
-        thread::sleep(time::Duration::from_millis(TICKER_SPEED));
-    }
-    pub fn print_map_animate(
-        columns: Vec<Vec<char>>,
-        map_height: usize,
-        map_width: usize,
-        starting_pos: (usize, usize),
-        starting_string: char,
-        colored_positions: HashMap<(usize, usize), (char, usize)>,
-    ) {
-        let (sx, sy) = starting_pos;
-        // Calculate the top-left corner of the viewport
-        let start_x = if sx > VIEWPORT_WIDTH / 2 {
-            sx - VIEWPORT_WIDTH / 2
-        } else {
-            0
-        };
-        let start_y = if sy > VIEWPORT_HEIGHT / 2 {
-            sy - VIEWPORT_HEIGHT / 2
-        } else {
-            0
-        };
-
-        // Clip the viewport to the grid boundaries
-        let end_x = (start_x + VIEWPORT_WIDTH).min(map_width);
-        let end_y = (start_y + VIEWPORT_HEIGHT).min(map_height);
-
-        // Clear the map
-        print!("\x1B[2J\x1B[1;1H");
-        let mut line_to_print = vec![];
-        for y_i in start_y..end_y {
-            let mut line: Vec<String> = vec![];
-            for x_i in start_x..end_x {
-                // If the position matches the cursor, display the override character
-
-                if (x_i, y_i) == (sx, sy) {
-                    // let start_char = format!(
-                    //     "{}{}{}",
-                    //     "\x1b[31m",
-                    //     &starting_string.to_string(),
-                    //     "\x1b[39m"
-                    // );
-                    line.push(format!(
-                        "{}{}{}",
-                        "\x1b[31m",
-                        &starting_string.to_string(),
-                        "\x1b[39m"
-                    ));
-                    continue;
-                }
-                // if extra_obstacle_pos
-                //     .as_ref()
-                //     .is_some_and(|obst_pos| obst_pos.contains(&(x_i, y_i)))
-                match colored_positions.get(&(x_i, y_i)) {
-                    Some((pos_char, _)) => {
-                        line.push("\x1b[31m".to_string() + &pos_char.to_string() + "\x1b[39m");
-                        continue;
-                    }
-                    _ => {}
-                }
-                // Default to the grid character
-                line.push(columns[x_i][y_i].to_string());
-            }
-            // Print the new line
-            line_to_print.push(line.join(""));
-            // line_to_print.push("\n".to_string());
-        }
-
-        print!("{}", line_to_print.join("\n"));
-        if PAUSE_ON_EACH_FRAME {
-            wait_for_input(false);
-        }
+fn safe_access<T>(grid: &Grid<T>, pos: (isize, isize)) -> Option<&T> {
+    if pos.0 >= 0 && pos.1 >= 0 && (pos.0 as usize) < grid.rows() && (pos.1 as usize) < grid.cols()
+    {
+        Some(&grid[(pos.0 as usize, pos.1 as usize)])
+    } else {
+        None
     }
 }
 
-fn main() {
-    // Calculate area and perimiter.
-    // Area = tile count.
-    // Perimiter per block = 4 - neighbour count
+fn move_in_grid<T>(grid: &Grid<T>, pos: (isize, isize), delta: (isize, isize)) -> Option<&T> {
+    let new_row = pos.0 + delta.0;
+    let new_col = pos.1 + delta.1;
+    safe_access(grid, (new_row, new_col))
+}
 
-    // Go row by row. Find a new starting position. Add it to a map.
-    // From that starting position, go row by row.
-    // For each matching tile, count the neighbours and collect if they have at least 1.
-    // When you encounter a row without any neighbors, break and start over.
+fn is_valid_move(grid: &Grid<char>, pos: (isize, isize), delta: (isize, isize)) -> bool {
+    let mark = move_in_grid(grid, pos, delta);
+    match mark {
+        Some('.') => true,
+        _ => false,
+    }
+}
 
+fn main() -> io::Result<()> {
+    const DIRECTIONS: [(isize, isize); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
+    const TEST: bool = true;
+    const DEBUG: bool = true;
     let file_path = if TEST {
         "./test_input.txt"
     } else {
         "./input.txt"
     };
     println!("In file {file_path}");
-    let mut columns: Vec<Vec<char>> = Vec::new();
 
-    for line in read_to_string(file_path).unwrap().lines() {
-        let mut row = vec![];
-        for char in line.chars() {
-            row.push(char);
+    let file = File::open(file_path)?;
+    let mut reader = BufReader::new(file);
+    let mut row_length = 0;
+    let mut buffer: [u8; 1] = [0; 1]; // Single-byte buffer
+    while reader.read(&mut buffer)? > 0 {
+        if buffer[0] == b'\n' {
+            break;
         }
-        columns.push(row);
+        row_length += 1;
     }
-    let map_height = columns[0].len();
-    let map_width = columns.len();
-    let starting_pos = (0, 0);
-    let starting_string = columns[starting_pos.0][starting_pos.1];
-    let region: HashMap<(usize, usize), (char, usize)> = HashMap::new();
-    // let starting_string =
 
-    print_map_animate(
-        columns,
-        map_height,
-        map_width,
-        starting_pos,
-        starting_string,
-        region,
-    );
+    let content = read_to_string(file_path)?;
+    let content_vec: Vec<char> = content.chars().filter(|&val| val != '\n').collect();
+
+    let char_grid = Grid::from_vec(content_vec, row_length);
+    println!("Row Length: {}", row_length);
+
+    let mut cells: Vec<Cell> = vec![];
+    for row_index in 0..char_grid.rows() {
+        for col_index in 0..char_grid.cols() {
+            // Make cell.
+            let mut cell = Cell {
+                col: col_index,
+                row: row_index,
+                height: 0,
+                valid_neighbors: vec![],
+            };
+            for delta in DIRECTIONS {
+                if is_valid_move(&char_grid, (row_index as isize, col_index as isize), delta) {
+                    let neighbor_pos = (row_index as isize + delta.0, col_index as isize + delta.1);
+                    cell.valid_neighbors
+                        .push((neighbor_pos.0 as usize, neighbor_pos.1 as usize));
+                }
+            }
+            cells.push(cell);
+        }
+    }
+
+    let cell_grid = Grid::from_vec(cells, row_length);
+
+    return Ok(());
+    // Get width:
+
+    // let grid = Grid::from_vec(read_to_string(file_path).unwrap(), 10);
+    // for char in line.chars() {}
 }
